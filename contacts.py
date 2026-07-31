949d12873cb7d449b4ccbffab63d67dc87f69d20
Levan Elikashvili <elikashvili.levan.cs@gmail.com>
2026-07-31 20:27:36 +0400

    Add contact retrival

--- contacts.py
+++ contacts.py
@@ -16,3 +16,21 @@ def add_contact(name, email):
 
     cursor.close()
     connection.close()
+
+def get_contacts():
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        SELECT * 
+        FROM contacts;
+        """
+    )
+
+    contacts = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return contacts  
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from contacts import add_contact
+from contacts import add_contact, get_contacts
+
+contacts = get_contacts()
+
+for contact in contacts:
+    print(contact)
 
 name = input("Name: ")
 email = input("Email: ")
