8d826734b4b4b0c5e3bc423d49b0eb8c62fcaf6e
Levan Elikashvili <elikashvili.levan.cs@gmail.com>
2026-08-01 14:46:14 +0400

    Complete contact CRUD operations

--- contacts.py
+++ contacts.py
@@ -34,3 +34,74 @@ def get_contacts():
     connection.close()
 
     return contacts  
+
+def update_contact_name(contact_id, new_name):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        UPDATE contacts
+        SET name = %s
+        WHERE contact_id = %s
+        """,
+        (new_name, contact_id)
+    )
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+def update_contact_email(contact_id, new_email):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        UPDATE contacts
+        SET email = %s
+        WHERE contact_id = %s
+        """,
+        (new_email, contact_id)
+    )
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+def update_contact_name_email(contact_id, new_name, new_email):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        UPDATE contacts
+        SET name = %s, email = %s
+        WHERE contact_id = %s
+        """,
+        (new_name, new_email, contact_id)
+    )
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+def delete_contact(contact_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        DELETE FROM contacts
+        WHERE contact_id = %s
+        """,
+        (contact_id,)
+    )
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
--- main.py
+++ main.py
@@ -1,13 +1,92 @@
-from contacts import add_contact, get_contacts
+from contacts import *
 
-contacts = get_contacts()
 
-for contact in contacts:
-    print(contact)
+def show_menu():
+    print(
+        "\nEnter 1 to add contact"
+        "\nEnter 2 to show contacts"
+        "\nEnter 3 to update contact"
+        "\nEnter 4 to delete contact"
+        "\nEnter 5 to exit program"
+    )
 
-name = input("Name: ")
-email = input("Email: ")
 
-add_contact(name, email)
+def menu():
+    while True:
+        show_menu()
+        menu_choice = input("\nEnter here: ")
 
-print("Contact added")
+
+        if menu_choice == '1':
+            name = input("\nEnter name here: ")
+            email = input("Enter email here: ")
+            add_contact(name, email)
+            print("\nContact added")
+
+
+        elif menu_choice == '2':
+            contacts = get_contacts()
+            if not contacts:
+                print("No contacts")
+                continue
+            for contact in contacts:
+                print(
+                    f"\nID: {contact[0]}"
+                    f"\nContact name: {contact[1]}"
+                    f"\nContact email: {contact[2]}"
+                )
+
+
+        elif menu_choice == '3':
+            contacts = get_contacts()
+            if not contacts:
+                print("No contacts")
+                continue
+            for contact in contacts:
+               print(
+                    f"\nID: {contact[0]}"
+                    f"\nContact name: {contact[1]}"
+                    f"\nContact email: {contact[2]}"
+                ) 
+            contact_id = int(input("\nEnter contact ID you want to update here: "))
+            print(
+                "\nEnter 1 to update contact name"
+                "\nEnter 2 to update contact email"
+                "\nEnter 3 to update contact "
+            )
+            update_choice = input("\nEnter here: ")
+            if update_choice == '1':
+                new_name = input("\nEnter new name here: ")
+                update_contact_name(contact_id, new_name)
+                print("\nContact name updated")
+            elif update_choice == '2':
+                new_email = input("\nEnter new email here: ")
+                update_contact_email(contact_id, new_email)
+                print("\nContact email updated")
+            elif update_choice == '3':
+                new_name = input("\nEnter new name here: ")
+                new_email = input("Enter new email here: ")
+                update_contact_name_email(contact_id, new_name, new_email)
+                print("\nContact name and email updated")
+
+
+        elif menu_choice == '4':
+            contacts = get_contacts()
+            if not contacts:
+                print("No contacts")
+                continue
+            for contact in contacts:
+                print(
+                    f"\nID: {contact[0]}"
+                    f"\nContact name: {contact[1]}"
+                    f"\nContact email: {contact[2]}"
+                )
+            contact_id = input("\nEnter contact ID you want to delete here: ")
+            delete_contact(contact_id)
+            print("Contact deleted")
+            
+        elif menu_choice == '5':
+            print("\nQuitting")
+            break
+
+menu()
